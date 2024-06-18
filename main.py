575b5c995c3b84999dbd5f8eaf1385bbb4233e15
Hubert <hubert.siemieniec@gmail.com>
2024-06-18 14:13:59 +0200

    Password generator code + .gitignore updates

---
+++ main.py
@@ -0,0 +1,33 @@
+# Password Generator Project
+from random import shuffle, choice
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '@']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+if nr_letters < 1 and nr_symbols < 1 and nr_numbers < 1:
+    print("Invalid symbols, letters and numbers number! You've to have at least one character in password!")
+else:
+    password_list, password = [], ""
+
+    for i in range(nr_letters):
+        password_list.append(choice(letters))
+
+    for i in range(nr_numbers):
+        password_list.append(choice(numbers))
+
+    for i in range(nr_symbols):
+        password_list.append(choice(symbols))
+
+    shuffle(password_list)
+
+    for char in password_list:
+        password += char
+
+    print(f"Your generated password is {password}")
